77e08bdd81d6f2df5569e9bfa7475fec9a55c37b
AnmoldeepBrar <anmoldeepkaur2910@gmail.com>
2024-04-02 15:01:42 -0500

    Changing root

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,16 +5,43 @@ def product_params
     params.require(:product).permit(:name, :description, :price, category_ids: [])
   end
 
-  def index
-    @products = Product.all
-    @categories = Category.all
+  # def index
+  #   @products = Product.all
+  #   @categories = Category.all
   
+  
+  #   respond_to do |format|
+  #     format.html # Render HTML by default (using index.html.erb)
+  #     format.json { render json: @products } # Render JSON for API requests
+  #   end
+  # end
+
+  def index
+    #@products = Product.all  # Retrieve all products (you might want to limit or filter this based on your requirements)
+    @products = Product.all.page(params[:page]).per(10)
+    @categories = Category.all
+
+    @products_on_sale = Product.where(on_sale: true)
+    @recently_updated_products = Product.order(updated_at: :desc).limit(3)
+
+    if params[:category_id].present?
+      @products = @products.in_category(params[:category_id])
+    end
+  
+    if params[:keyword].present?
+      @products = @products.search_by_keyword(params[:keyword])
+    end
   
     respond_to do |format|
       format.html # Render HTML by default (using index.html.erb)
       format.json { render json: @products } # Render JSON for API requests
     end
   end
+
+  def search
+    redirect_to products_path(keyword: params[:keyword], category_id: params[:category_id])
+  end
+
 
   def show
     @product = Product.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  root 'home#index'
+  root 'products#index'
   resources :products
   resources :products, only: [:show]
   resources :products, only: [:index]
