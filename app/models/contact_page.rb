059776e350aea02e910075b83dec83e53a819915
AnmoldeepBrar <anmoldeepkaur2910@gmail.com>
2024-04-19 02:44:13 -0500

    Model is fixed

--- app/models/contact_page.rb
+++ app/models/contact_page.rb
@@ -3,6 +3,5 @@ class ContactPage < ApplicationRecord
 
     def self.ransackable_attributes(auth_object = nil)
         ["content", "created_at", "id", "id_value", "updated_at"]
-        # Add any additional attributes here that you want to be searchable
       end
 end
