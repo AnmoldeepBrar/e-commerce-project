c0b10a923254417f677b9d133d888db8e698e808
AnmoldeepBrar <anmoldeepkaur2910@gmail.com>
2024-04-19 02:42:14 -0500

    Changes are made in category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
     add_breadcrumb "Home", :root_path
-    # Controller actions
+    
     def index
         add_breadcrumb "Categories", categories_path
         add_breadcrumb "Products", list_products_path
